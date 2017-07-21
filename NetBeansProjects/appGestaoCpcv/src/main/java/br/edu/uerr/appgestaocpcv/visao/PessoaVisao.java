f7209917dbe206f10280a2243f95eb77a8ab4abf
fpcarlos <fpcarlos@fpcarlos-not>
2017-07-21 01:45:05 -0400

    Projeto Inicial

---
+++ NetBeansProjects/appGestaoCpcv/src/main/java/br/edu/uerr/appgestaocpcv/visao/PessoaVisao.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.uerr.appgestaocpcv.visao;
+
+import br.edu.uerr.appgestaocpcv.controle.EnderecoPessoaControle;
+import br.edu.uerr.appgestaocpcv.controle.PessoaControle;
+import br.edu.uerr.appgestaocpcv.modelo.EnderecoPessoa;
+import br.edu.uerr.appgestaocpcv.modelo.Pessoa;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author fpcarlos
+ */
+@Named
+@SessionScoped
+public class PessoaVisao extends AbstractVisao implements Serializable{
+    
+    @EJB
+    private PessoaControle pessoaControle;
+    
+    @EJB
+    private EnderecoPessoaControle enderecoPessoaControle;
+    
+    private Pessoa pessoa;
+    
+    private EnderecoPessoa enderecoPessoa;
+    
+    private List<Pessoa> listPessoa = new ArrayList<>();
+    
+    private List<EnderecoPessoa> listEnderecoPessoa = new ArrayList<>();
+    
+    public PessoaVisao(){
+        super();
+    }
+    
+        public String abrirCadastro() {
+        try {
+            pessoa = new Pessoa();
+            listPessoa = new ArrayList<>();
+            enderecoPessoa = new EnderecoPessoa();
+            listEnderecoPessoa = new ArrayList<>();            
+
+            listPessoa = pessoaControle.findAll();
+            listEnderecoPessoa = enderecoPessoaControle.findAllPeloIdPessoa(pessoa.getId());
+            return redirect("/sistema/usuario/formCandidato.xhtml");
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void salvar() {
+        try {
+            pessoaControle.salvar(pessoa);
+
+            showFacesMessage("salvo com sucesso!!!", 2);
+            pessoa = new Pessoa();
+            listPessoa = new ArrayList<>();
+            listPessoa = pessoaControle.findAll();
+            //FacesContext.getCurrentInstance().getExternalContext().redirect("/evento/sistema/usuario/listaUsuarios.xhtml");
+            //return redirect("/sistema/usuario/listaUsuarios.xhtml?faces-redirect=true");
+        } catch (Exception e) {
+            showFacesMessage(e.getMessage(), 4);
+            //return null;
+        }
+    }
+
+    public String editar(Pessoa aux) {
+        try {
+            pessoa = pessoaControle.pegaPessoaId(aux.getId());
+
+            listPessoa = new ArrayList<>();
+            listPessoa = pessoaControle.findAll();
+            return redirect("/sistema/usuario/formCandidato.xhtml");
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String remover(Pessoa aux) {
+        try {
+            //pessoaControle.remove(aux);
+
+            showFacesMessage("Impossível remover candidato!!!", 4);
+            pessoa = new Pessoa();
+            listPessoa = new ArrayList<>();
+            listPessoa = pessoaControle.findAll();
+
+            return redirect("/sistema/usuario/formCandidato.xhtml");
+
+        } catch (Exception e) {
+            showFacesMessage(e.getMessage(), 4);
+            return null;
+        }
+    }
+
+    
+    
+    //Gets e Sets
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public List<Pessoa> getListPessoa() {
+        return listPessoa;
+    }
+
+    public void setListPessoa(List<Pessoa> listPessoa) {
+        this.listPessoa = listPessoa;
+    }
+
+    public EnderecoPessoa getEnderecoPessoa() {
+        return enderecoPessoa;
+    }
+
+    public void setEnderecoPessoa(EnderecoPessoa enderecoPessoa) {
+        this.enderecoPessoa = enderecoPessoa;
+    }
+
+    public List<EnderecoPessoa> getListEnderecoPessoa() {
+        return listEnderecoPessoa;
+    }
+
+    public void setListEnderecoPessoa(List<EnderecoPessoa> listEnderecoPessoa) {
+        this.listEnderecoPessoa = listEnderecoPessoa;
+    }
+    
+    
+    
+}
